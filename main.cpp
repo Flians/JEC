028f6b3dd557c283ec631035e51cda3241101703
Flians <rongliangfu@outlook.com>
2020-09-12 17:46:15 +0800

    update evaluate_opensmt

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         simplify sim;
         sim.clean_wire_buf(miter.PIs);
         vector<vector<Node *>> &layers = sim.id_reassign_and_layered(miter.PIs, miter.POs);
-        sim.reduce_repeat_nodes(layers); // no considering the positions of ports for DC and HUMX
+        sim.reduce_repeat_nodes(layers);
         sim.id_reassign(layers);
         endTime = clock();
         cout << "The simplify time is: " << (double)(endTime - startTime) / CLOCKS_PER_SEC << " S" << endl;
